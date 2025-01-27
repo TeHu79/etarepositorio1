ed79f36f3c50d6b6efc18422925a7e81910f51b9
Teija Hurme <teija.hurme@student.careeria.fi>
2025-01-27 21:17:06 +0200

    Lisättiin uusi metodi koodi.java -tiedostoon

--- koodi.java
+++ koodi.java
@@ -5,3 +5,9 @@ Uusi rivi GitHubissa - Develop branchiin
 Tekoäly 1
 Tekoäly 2
 Tekoäly 3
+
+Käytetään toteutettua nettiyhteyttä.
+
+void TulostetaanHeiMaailma() {
+   System.out.pringln("Hei, maailma!");
+}
